7a5c7b6db5502530a4cc0ae15642885b75a3a7a7
langm <lang.marius@t-online.de>
2021-04-08 22:15:45 +0200

    bugfix easter.js

--- src/easter.js
+++ src/easter.js
@@ -5,15 +5,15 @@ const formatDE = 'DE';
 
 // eslint-disable-next-line consistent-return
 Easter.calcEasterSunday = (format, year) => {
-  const a = year % 19;
+  const a = year % 19; // currentYear mod 19
   const b = (19 * a + 24) % 30;
-  const c = b + (2 * (year % 4) + 4 * (year % 7) + 6 * b + 5);
-  let easterSundayDay = c % 7;
+  const c = (2 * (year % 4) + 4 * (year % 7) + 6 * b + 5) % 7;
+  let easterSundayDay = b + c;
 
   if (easterSundayDay === 35 || (easterSundayDay === 34 && b === 28 && a > 10)) {
     easterSundayDay -= 7;
   }
-
+  // generate easterDate and change Parameters to easter Date
   const easterDate = new Date(year, 2, 22);
   easterDate.setTime(easterDate.getTime() + 86400000 * easterSundayDay);
   const easterMonth = easterDate.getMonth() + 1;
